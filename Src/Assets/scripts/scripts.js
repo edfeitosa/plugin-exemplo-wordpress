19162a561a700e2e9dcfb8a3464b090263040435
Eduardo Domingues Feitosa <eduardo.feitosa@localiza.com>
2020-11-10 13:56:46 -0300

    modal component adjustment

--- Src/Assets/scripts/scripts.js
+++ Src/Assets/scripts/scripts.js
@@ -8,33 +8,60 @@ function substituirTermo(texto, termo_antigo, termo_novo) {
 	return texto.replace(termo_antigo, termo_novo);
 }
 
-function modal(acao) {
-	let bgModal = document.getElementById('bg-modal');
-	bgModal.style.display = acao;
+function modal(acao = 'none', mensagem = 'mensagem do modal', estilo = 'cabecalho-sucesso') {
+	let titulo = {
+		'cabecalho-sucesso': 'Sucesso',
+		'cabecalho-erro': 'Erro',
+		'cabecalho-alerta': 'Alerta'
+	}
+
+	let modal = document.getElementById('bg-modal');
+	modal.style.display = acao;
+	let cabecalho = document.getElementById('cabecalho');
+	cabecalho.setAttribute('class', estilo);
+	cabecalho.innerHTML = titulo[estilo];
+	document.getElementById('body').innerHTML = mensagem;
+	document.getElementById('fechar').onclick = function() {
+		modal.style.display = 'none';
+	}
 }
 
 function salvaEdicao() {
 	document.getElementById("salvarEdicao").onclick = function() {
-		let titulo = document.getElementById("titulo").value;
-		let url = document.getElementById("url").value;
-		let status = document.getElementById("status").value;
+		let titulo = document.getElementById("titulo");
+		let url = document.getElementById("url");
+		let status = document.getElementById("status");
 		let identificador = document.getElementById("identificador").value;
 		let servidor = document.getElementById("servidor").value;
 		let uri = document.getElementById("uri").value;
-		let ajax = new XMLHttpRequest();
-		ajax.open("POST", servidor + uri, true);
-		ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		ajax.send("titulo=" + titulo + "&url=" + url + "&status=" + status + "&identificador=" + identificador);
-		if (ajax.status == 200) {
-			modal('flex');
+
+		if (titulo == '' || url == '') {
+			modal('flex', 'Os campos marcados com (*), são obrigatórios', 'cabecalho-erro');
 		} else {
-			modal('flex');
+			let ajax = new XMLHttpRequest();
+			ajax.open("POST", servidor + uri, true);
+			ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			ajax.send("titulo=" + titulo.value + "&url=" + url.value + "&status=" + status.value + "&identificador=" + identificador);
+			
+			ajax.onreadystatechange = function() {
+				if (ajax.status == 200) {
+					if (identificador == '0') {
+						titulo.value = '';
+						url.value = '';
+						status.value = '1';
+					}
+					modal('flex', 'Os dados foram salvos com sucesso', 'cabecalho-sucesso');
+					voltaParaConsulta('fechar');
+				} else {
+					modal('flex', 'Ocorreu um erro e não possível salvar', 'cabecalho-erro');
+				}
+			}
 		}
 	}
 }
 
-function voltaParaConsulta() {
-	document.getElementById("cancelarEdicao").onclick = function() {
+function voltaParaConsulta(identificador) {
+	document.getElementById(identificador).onclick = function() {
 		let servidor = document.getElementById("servidor").value;
 		let uri = document.getElementById("uri").value;
 		let url_atual = servidor + uri;
@@ -44,7 +71,7 @@ function voltaParaConsulta() {
 }
 
 window.onload = function() {
-	modal('none');
+	modal();
 	salvaEdicao();
-	voltaParaConsulta();
+	voltaParaConsulta('cancelarEdicao');
 }
