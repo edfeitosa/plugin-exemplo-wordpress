96dc2607096d4703f7d616db409d2fa0de8c7747
Eduardo Domingues Feitosa <eduardo.feitosa@localiza.com>
2020-12-01 23:03:18 -0300

    sanitize string -> endpoint

--- Src/Source/Sites.class.php
+++ Src/Source/Sites.class.php
@@ -2,6 +2,18 @@
 namespace Source;
 
 class Sites {
+
+	private function sanitizeString($str) {
+    $str = preg_replace('/[áàãâä]/ui', 'a', $str);
+    $str = preg_replace('/[éèêë]/ui', 'e', $str);
+    $str = preg_replace('/[íìîï]/ui', 'i', $str);
+    $str = preg_replace('/[óòõôö]/ui', 'o', $str);
+    $str = preg_replace('/[úùûü]/ui', 'u', $str);
+    $str = preg_replace('/[ç]/ui', 'c', $str);
+    $str = preg_replace('/[^a-z0-9]/i', '_', $str);
+    $str = preg_replace('/_+/', '-', $str);
+    return $str;
+	}
 	
 	public static function auth() {
 		// http://localhost/estudo-plugins-wordpress/wp-json/liberasite/v1/autorizacao?id=9
@@ -22,7 +34,7 @@ class Sites {
           $wpdb->prefix . PREFIX_PLUGIN . 'sites',
           array(
 						'sit_title' => $_POST['titulo'],
-						'sit_endpoint' => $_POST['endpoint'],
+						'sit_endpoint' => self::sanitizeString($_POST['endpoint']),
 						'sit_auth_code' => md5(date(DATE_RFC822)),
 						'sit_status' => $_POST['status'],
 						'sit_user' => get_current_user_id(),
