2e894b68419f2ee724ba19e5cc44d684f947f9de
Eduardo Domingues Feitosa <eduardo.feitosa@localiza.com>
2020-12-20 18:48:37 -0300

    change logo: localiza -> labs

--- Src/Shared/Header.class.php
+++ Src/Shared/Header.class.php
@@ -6,7 +6,7 @@ class Header {
 	public static function header() {
 		$html = "
 			<div class='cabecalho-plugin'>
-				<img src='" . WP_PLUGIN_URL . "/endpoints/Src/Assets/img/Localiza.png' title='Localiza' alt='Localiza' />
+				<img src='" . WP_PLUGIN_URL . "/endpoints/Src/Assets/img/Labs.png' title='Labs' alt='Labs' width='150px' />
 			</div>
 		";
 		return $html;
